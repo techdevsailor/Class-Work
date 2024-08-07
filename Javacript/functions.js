97c790262d9502a285f20188a5e6d5c24bbdbb34
techdevsailor <techdevsailor@gmail.com>
2024-08-07 05:38:36 -0700

    nestic function

--- Javacript/functions.js
+++ Javacript/functions.js
@@ -1,9 +1,24 @@
-function myclass(){
-    let laptops = 2 + 3;
+// function myclass(){
+//     let laptops = 2 + 3;
     
-}
+// }
 
-console.log(myclass())
+// console.log(myclass())
 
+let num1 = 20
+let num2 = 30
+let name = "Abubakar"
+
+function result(){
+    let num1 = 10
+    let num2 = 11
+
+    function multiply(){
+        return `${name} result ${num1 * num2}`
+
+    }
+    return multiply() 
+    }
+    console.log(result())
 
 
