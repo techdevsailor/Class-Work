86371e1b8a503fd34271057cbca6b370ee0e652c
techdevsailor <techdevsailor@gmail.com>
2024-08-08 03:16:45 -0700

    array methods

--- Javacript/functions.js
+++ Javacript/functions.js
@@ -1,56 +1,55 @@
-// function myclass(){
-//     let laptops = 2 + 3;
+// // function myclass(){
+// //     let laptops = 2 + 3;
     
+// // }
+
+// // console.log(myclass())
+
+//         // let num1 = 20       //global veriable
+//         // let num2 = 30       ////global veriable
+//         // let name = "Abubakar"   //global veriable
+
+//         // function result(){
+//         //     let num1 = 10
+//         //     let num2 = 11
+
+//         //     function multiply(){    //Nestic function
+//         //         return `${name} result ${num1 * num2}`
+
+//         //     }
+//         //     return multiply() 
+//         //     }
+//         //     console.log(result())
+
+
+//         //     //arrow function..............
+
+//         //     const sum = (x,y) => x + y
+
+//         //     console.log(sum(100,100))
+
+//     //Array push method (Appends new elements to the end of an array, and returns the new length of the array)................
+//                 // let fruit = ['Apple', 'Banana'];
+//                 // fruit.push('Orange', 'Mango')
+//                 // console.log(fruit)
+
+
+//     //Array pop method (remove new elements from the end of an array, and returns the new length of the array)................
+//             // let fruit = ['Apple', 'Banana'];
+//             // fruit.pop()
+//             // console.log(fruit)
+
+//             // //Array forEach method............
+//             // let fruit = ['Apple', 'Banana'];
+//             // fruit.forEach((fruit) => {
+//             // console.log(fruit)
+//             // })
+
+//     //Loops.......
+// for(let i = 0; i < 5: i++){
+//     console.log("Number:" + i)
+
 // }
-
-// console.log(myclass())
-
-        // let num1 = 20       //global veriable
-        // let num2 = 30       ////global veriable
-        // let name = "Abubakar"   //global veriable
-
-        // function result(){
-        //     let num1 = 10
-        //     let num2 = 11
-
-        //     function multiply(){    //Nestic function
-        //         return `${name} result ${num1 * num2}`
-
-        //     }
-        //     return multiply() 
-        //     }
-        //     console.log(result())
-
-
-        //     //arrow function..............
-
-        //     const sum = (x,y) => x + y
-
-        //     console.log(sum(100,100))
-
-    //Array push method (Appends new elements to the end of an array, and returns the new length of the array)................
-                // let fruit = ['Apple', 'Banana'];
-                // fruit.push('Orange', 'Mango')
-                // console.log(fruit)
-
-
-    //Array pop method (remove new elements from the end of an array, and returns the new length of the array)................
-            // let fruit = ['Apple', 'Banana'];
-            // fruit.pop()
-            // console.log(fruit)
-
-            // //Array forEach method............
-            // let fruit = ['Apple', 'Banana'];
-            // fruit.forEach((fruit) => {
-            // console.log(fruit)
-            // })
-
-    //Loops.......
-for(let i = 0; i < 5: i++){
-    console.log("Number:" + i)
-
-}
-
 
 
 
